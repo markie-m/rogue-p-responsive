e4f08935e96d6ce4fdc21352a5d91fbf88bede54
markie_m <marzolfmarkie@gmail.com>
2021-07-10 08:14:29 -0500

    Spread out the button links on the About page design

--- js/rogue-p.js
+++ js/rogue-p.js
@@ -1,3 +1,20 @@
+// This is the code that makdes the Locations page animated globe icon function
+function globespin() {
+  var a;
+  a = document.getElementById("globe");
+  a.innerHTML = "&#xf57d;";
+  setTimeout(function () {
+      a.innerHTML = "&#xf57e;";
+    }, 1000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf57c;";
+    }, 2000);
+}
+globespin();
+setInterval(globespin, 3000);
+// End of Locations page animated icon JS
+
+
 // This is the code that makdes the Gallery page slideshow function
 var slideIndex = 1;
 showSlides(slideIndex);
